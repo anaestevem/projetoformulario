606dcbe75a3064f40bd75fcc558a7eaac73fd489
anaestevem <ana.estevem@escola.pr.gov.br>
2026-04-24 10:55:26 -0300

    anaa

---
+++ script.js
@@ -0,0 +1,8 @@
+function salvarCarro(event) {
+     event.preventdefault();
+
+     let titulo = document.getElementById(´title).ariaValueMax;
+     let preco = document.getElementById(´preco´)
+     let marca = document.getElementById(´marca´)
+     let modelo = document.getElementById(´modelo´)
+}
